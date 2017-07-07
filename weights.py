398eb130ca426e6b091058f3fff896718ab9c097
aijams <aijams@umass.edu>
2017-07-07 11:40:59 -0400

    Added pt and dt weight formulas for single powers and updated multi-power formulas appropriately. Fixed bug with miscalculation of weights for pt-side partitions

--- weights.py
+++ weights.py
@@ -11,8 +11,20 @@ from sage.combinat.partition import Partition
 from sage.combinat.skew_tableau import SkewTableau
 from reverse_plane_partition import ReversePlanePartitions
 
-def partition_weight(coefficients, powers, partition):
-    """Calculate the weight of the partition."""
+def partition_weight(coefficients, powers, partition, side='dt'):
+    """Calculate the weight of the partition. Argument side defaults to 'dt' and should be
+       set to 'dt' for SkewPlanePartitions and 'pt' for ReversePlanePartitions. 
+
+       Args:
+           coefficients: A three element tuple (a,b,c) for changing the weight formula
+           powers: A collection k of integers k_1, ..., k_n for computing the weight
+           partition: A partition to compute the weight of
+           side: A string specifying whether to compute the weight for a ReversePlanePartition('pt')
+                 or a SkewPlanePartition('dt')
+
+       Returns:
+           The weight of the partition (an integer) given the powers and coefficients.
+    """
     a, b, c = coefficients
     try:
         m = Integer(powers)
@@ -24,14 +36,55 @@ def partition_weight(coefficients, powers, partition):
             raise TypeError(
                 "Powers must be an integer or iterable")
         # product over all elements of powers
-        return prod(partition_weight(coefficients, m, partition)
+        return prod(partition_weight(coefficients, m, partition, side=side)
                     for m in powers_tuple)
 
     partition_tab = SkewTableau(partition)
     # m is now the only power
-    return sum((a * i) ** m + (b * j) ** m + (c * k) ** m
-               for i, j in partition_tab.cells()
-               for k in range(partition_tab[i][j]))
+    if side == 'dt':
+        return dt_weight_formula(coefficients, m, partition_tab)
+    else:
+        return pt_weight_formula(coefficients, m, partition_tab)
+    
+def pt_weighted_sum(coefficients, powers, shape, prec, base = None):
+    """Calculate the weighted sum over weight*q^size"""
+    if base is None:
+        base = ZZ
+    R = PowerSeriesRing(base, 'q')
+    q = R.gen()
+    return sum(
+        partition_weight(coefficients, powers, part, side='pt') * q ** size
+        for size in range(prec)
+        for part in ReversePlanePartitions(shape, size)) + R([], prec)
+
+def pt_weighted_sum_abc(powers, shape, prec):
+    """Calculate the sum, but leave in symbols a, b, c"""
+    base = PolynomialRing(ZZ, 'a,b,c')
+    return pt_weighted_sum(base.gens(), powers, shape, prec, base)
+
+def dt_weight_formula(coefficients, power, partition):
+    return sum((a * i) ** power + (b * j) ** power + (c * k) ** power
+               for i, j in partition.cells()
+               for k in range(partition[i][j]))
+
+def pt_weight_formula(coefficients, power, partition):
+    return sum((a * i) ** power + (b * j) ** power + (c * (-1-k)) ** power
+               for i, j in partition.cells()
+               for k in range(partition[i][j]))
+
+def dt_weighted_sum(coefficients, powers, shape, prec, base=None):
+    if base is None:
+        base = ZZ
+    R = PowerSeriesRing(base, 'q')
+    q = R.gen()
+    return sum(
+        negatives = [-value for value in coefficients]
+        partition_weight(negatives, powers, part, side='dt') * q**size
+        for size in range(prec)
+        for part in SkewPlanePartitions(shape, size)) + R([], prec)
+
+# Temp Stuff for Skew Plane Partitions
+
 
 def SkewPlanePartitions(outer_shape, inner_shape, n):
     """ Generate a list of all skew plane partitions"""
@@ -48,23 +101,3 @@ def funcToPlanePartition(func):
     """
     shape = [len(row) for row in func]
     current = [row[-1] for row in func]
-    
-def pt_weighted_sum(coefficients, powers, shape, prec, base = None):
-    """Calculate the weighted sum over weight*q^size"""
-    if base is None:
-        base = ZZ
-    R = PowerSeriesRing(base, 'q')
-    q = R.gen()
-    return sum(
-        partition_weight(coefficients, powers, part) * q ** size
-        for size in range(prec)
-        for part in ReversePlanePartitions(shape, size)) + R([], prec)
-
-def pt_weighted_sum_abc(powers, shape, prec):
-    """Calculate the sum, but leave in symbols a, b, c"""
-    base = PolynomialRing(ZZ, 'a,b,c')
-    return pt_weighted_sum(base.gens(), powers, shape, prec, base)
-
-def dt_weighted_sum(coefficients, powers, shape, prec, base=None):
-    pass
-
