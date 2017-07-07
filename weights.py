4b8a155283031efa9cbeb8d3079886c3b802d71e
aijams <aijams@umass.edu>
2017-07-07 10:57:18 -0400

    added skew partition enumeration to weights.py, attempted fix of import statement for Hillman-Grassl-Algorithm in weights.py

--- weights.py
+++ weights.py
@@ -9,7 +9,7 @@ The index of (i, j, k) starts at (0, 0, 0).
 
 from sage.combinat.partition import Partition
 from sage.combinat.skew_tableau import SkewTableau
-from hillman_grassl import Inverse_HG
+from HG_stuff.Hillman_Grassl_Algorithm import Inverse_HG
 
 def partition_weight(coefficients, powers, partition):
     """Calculate the weight of the partition."""
@@ -46,6 +46,23 @@ def ReversePlanePartitions(shape, n):
             s += p
         yield Inverse_HG(hg)
 
+def SkewPlanePartitions(outer_shape, inner_shape, n):
+    """ Generate a list of all skew plane partitions"""
+    # The shape of the plane partitions to be generated
+    plane_shape = SkewPartition(outer_shape, inner_shape)
+    cells = plane_shape.cells()
+
+def funcToPlanePartition(func):
+    """use cumulative sum to convert functions from shape to non-negative integers
+       into skew plane partitions.
+
+       Args:
+         func: A list of lists containing non-negative integers.
+    """
+    shape = [len(row) for row in func]
+    current = [row[-1] for row in func]
+    
+
 def pt_weighted_sum(coefficients, powers, shape, prec, base = None):
     """Calculate the weighted sum over weight*q^size"""
     if base is None:
@@ -61,3 +78,7 @@ def pt_weighted_sum_abc(powers, shape, prec):
     """Calculate the sum, but leave in symbols a, b, c"""
     base = PolynomialRing(ZZ, 'a,b,c')
     return pt_weighted_sum(base.gens(), powers, shape, prec, base)
+
+def dt_weighted_sum(coefficients, powers, shape, prec, base=None):
+    pass
+
